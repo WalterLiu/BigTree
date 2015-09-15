1c6d6782c69f9bd9043c1826c4c0749bd089be01
walter <15110277689@163.com>
2015-09-15 14:35:23 +0800

    committed by Walter
    committed by Walter

--- HomePages/HomePages/ViewController.h
+++ HomePages/HomePages/ViewController.h
@@ -9,6 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *myBtn;
+- (IBAction)myBtnClicked:(id)sender;
 
 
 @end
--- HomePages/HomePages/ViewController.m
+++ HomePages/HomePages/ViewController.m
@@ -17,11 +17,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *title = @"abcdefdfdfjkfjfkdsajfkdjsfkjdkfjdkfjdkjfkdjfkdjfdjkfdjfkdjfkdjfkjdkfjdkafjdkjfkdjfkdjfdkjfkdjfkdjfkjdkfjkdfj";
+    [self.myBtn setTitle:title forState:UIControlStateNormal];
+    [self.myBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    self.myBtn.titleLabel.numberOfLines = 0;
+//    self.myBtn.titleLabel.lineBreakMode = NSLineBreakByTruncatingMiddle;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)myBtnClicked:(id)sender {
+}
+@end
+
+@interface UIButton (CustomeCategory)
+
+@end
+
+@implementation UIButton (CustomeCategory)
+
+//- (CGRect)contentRectForBounds:(CGRect)bounds {
+//    CGRect rect = bounds;
+//    rect.size.height = 10.0;
+//    NSLog(@"bounds: %@", NSStringFromCGRect(bounds));
+//    NSLog(@"rect: %@", NSStringFromCGRect(rect));
+//    return rect;
+//}
+
+- (CGRect)titleRectForContentRect:(CGRect)contentRect {
+    CGRect rect = contentRect;
+    rect.size.height = 40.0;
+    NSLog(@"bounds: %@", NSStringFromCGRect(contentRect));
+    NSLog(@"rect: %@", NSStringFromCGRect(rect));
+    return rect;
 }
 
 @end
